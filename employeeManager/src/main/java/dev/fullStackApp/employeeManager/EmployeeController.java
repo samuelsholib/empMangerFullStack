dfc5b92398155ca7523e40605d56c86b5cabfa6c
samuelsholib <samuelsholib12@gmail.com>
2022-09-15 14:32:34 -0700

    front-end initiated

--- employeeManager/src/main/java/dev/fullStackApp/employeeManager/EmployeeController.java
+++ employeeManager/src/main/java/dev/fullStackApp/employeeManager/EmployeeController.java
@@ -15,15 +15,15 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    // http://localhost:8080/employee/allEmployee
-    @GetMapping("allEmployee")
+    // http://localhost:8080/employee/getAllEmployees
+    @GetMapping("getAllEmployees")
     public ResponseEntity<List<Employee>> getAllEmployee() {
         List<Employee> employees = employeeService.findAllEmployee();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
-    // http://localhost:8080/employee/find/{id}
-    @GetMapping("find/{id}")
+    // http://localhost:8080/employee/findOne/id
+    @GetMapping("findOne/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
@@ -35,13 +35,13 @@ public class EmployeeController {
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
     // http://localhost:8080/employee/updateEmployee
-    @PutMapping("updateEmployee")
+    @PutMapping("/updateEmployee")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
         Employee updateEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
     // http://localhost:8080/employee/deleteEmployee/{id}
-    @DeleteMapping("deleteEmployee/{id}")
+    @DeleteMapping("/deleteEmployee/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable ("id")Long id) {
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.OK);
